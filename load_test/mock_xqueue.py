092c6b0cd61904e58d08a6a8700214b462fed773
Dave St.Germain <dstgermain@edx.org>
2014-05-05 11:20:46 -0400

    simple load test with mock xqueue server

---
+++ load_test/mock_xqueue.py
@@ -0,0 +1,96 @@
+import flask
+import itertools
+import random
+import time
+
+app = flask.Flask(__name__)
+
+counter = itertools.count()
+
+SUBMISSIONS = [
+('', 'ps03/derivatives/grade_derivatives.py'),
+('import time;time.sleep(100)', 'ps03/derivatives/grade_derivatives.py'),
+('''
+def foo():
+    return "hello"
+''', 'ps02/bisect/grade_bisect.py'),
+('''
+deff bad():
+    haha
+''', 'ps02/bisect/grade_bisect.py'),
+('''
+monthlyInterestRate = annualInterestRate/12
+lo = balance/12
+hi = (balance*(1+monthlyInterestRate)**12)/12
+done = False
+while not done :
+    payment = (lo+hi)/2
+    nb = balance
+    for m in range(0,12) :
+        nb = (nb-payment)*(1+monthlyInterestRate)
+    done = (abs(nb) < .005);
+    if (nb > 0) :
+        lo = payment
+    else :
+        hi = payment
+print('Lowest Payment: %.2f' % payment)
+''', 'ps02/bisect/grade_bisect.py'),
+]
+
+COUNTERS = {
+    'requests': 0,
+    'results': 0,
+    'start': time.time()
+}
+
+@app.route('/start')
+def start():
+    COUNTERS['start'] = time.time()
+    COUNTERS['requests'] = COUNTERS['results'] = 0
+    return flask.jsonify(COUNTERS)
+
+@app.route('/stats')
+def stats():
+    timediff = time.time() - COUNTERS['start']
+    response = {
+        'requests_per_second': COUNTERS['requests'] / timediff,
+        'posts_per_second': COUNTERS['results'] / timediff
+    }
+    return flask.jsonify(response)
+
+
+@app.route('/xqueue/get_submission/')
+def get_submission():
+    idx = random.randint(0, len(SUBMISSIONS) - 1)
+    submission, grader = SUBMISSIONS[idx]
+    payload = {
+        'grader': grader
+    }
+    response = {
+        'return_code': 0,
+        'content': flask.json.dumps({
+            'xqueue_header': '{}.{}'.format(counter.next(), idx),
+            'xqueue_body': flask.json.dumps({
+                'student_response': submission,
+                'grader_payload': flask.json.dumps(payload)
+            }),
+            'xqueue_files': ''
+        })
+    }
+    COUNTERS['requests'] += 1
+    return flask.jsonify(response)
+
+@app.route('/xqueue/login/', methods=['POST'])
+def login():
+    return flask.jsonify({'return_code': 0})
+
+
+@app.route('/xqueue/put_result/', methods=['POST'])
+def put_result():
+    COUNTERS['results'] += 1
+    return flask.jsonify({'return_code': 0, 'content': 'thank you'})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ load_test/run.py
@@ -0,0 +1,103 @@
+import threading
+import subprocess
+import os
+import sys
+import time
+import requests
+import json
+import tempfile
+import getpass
+from path import path
+import pprint
+import argparse
+
+
+WATCHER_CONFIG = {
+    "load-test": {
+        "SERVER": 'undefined',
+        "CONNECTIONS": 1,
+        "HANDLERS": [
+            {
+                "HANDLER": "xqueue_watcher.jailedgrader.JailedGrader",
+                "KWARGS": {
+                    "grader_root": path(__file__).dirname() / "../../data/6.00x/graders/",
+                }
+            }
+        ]
+    }
+}
+
+def start_mock_xqueue(port):
+    cmd = 'gunicorn -w 1 -k gevent -b 0.0.0.0:%s mock_xqueue:app' % port
+    print cmd
+    proc = subprocess.Popen(cmd.split())
+    return proc
+
+def start_queue_watcher(config_file, codejail_config_file):
+    cmd = 'python -m xqueue_watcher -f {} -j {}'.format(config_file, codejail_config_file)
+    print cmd
+    proc = subprocess.Popen(cmd.split())
+    return proc
+
+def get_stats(server_address):
+    pprint.pprint(requests.get('%s/stats' % server_address).json())
+    print '\n'
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--concurrency', default=2, type=int, help='number of watchers')
+    parser.add_argument('-x', '--xqueue', action="store_true", help='run mock xqueue', default=False)
+    parser.add_argument('-w', '--watcher', action="store_true", help='run watcher', default=False)
+    parser.add_argument('-a', '--xqueue-address', help='xqueue address', default='http://127.0.0.1:18042')
+    args = parser.parse_args(args)
+
+    if not (args.xqueue or args.watcher):
+        parser.print_help()
+        return -1
+
+    if args.xqueue:
+        port = args.xqueue_address.split(':')[-1]
+        xqueue_proc = start_mock_xqueue(port)
+
+        time.sleep(2)
+    else:
+        xqueue_proc = None
+
+    if args.watcher:
+        codejail_config = tempfile.NamedTemporaryFile(delete=False)
+        json.dump({'python_bin': sys.executable, 'user': getpass.getuser()}, codejail_config)
+        codejail_config.close()
+
+        watcher_config = tempfile.NamedTemporaryFile(delete=False)
+        WATCHER_CONFIG['load-test']['CONNECTIONS'] = args.concurrency
+        WATCHER_CONFIG['load-test']['SERVER'] = args.xqueue_address
+        json.dump(WATCHER_CONFIG, watcher_config)
+        watcher_config.close()
+        pprint.pprint(WATCHER_CONFIG)
+
+        watcher_proc = start_queue_watcher(watcher_config.name, codejail_config.name)
+        time.sleep(1)
+        print requests.get('%s/start' % args.xqueue_address).json()
+    else:
+        watcher_proc = None
+
+
+    while watcher_proc or xqueue_proc:
+        try:
+            time.sleep(2)
+            get_stats(args.xqueue_address)
+        except KeyboardInterrupt:
+            break
+
+    if watcher_proc:
+        os.kill(watcher_proc.pid, 15)
+        codejail_config.unlink(codejail_config.name)
+        watcher_config.unlink(watcher_config.name)
+
+    if xqueue_proc:
+        os.kill(xqueue_proc.pid, 15)
+
+    print '\n\ndone'
+    
+if __name__ == '__main__':
+    main(sys.argv[1:])
