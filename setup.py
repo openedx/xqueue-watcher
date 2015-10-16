17806f408700138f407f63fa05fdb4985ebe84d9
Edward Zarecor <ed@edx.org>
2015-10-16 14:32:05 -0400

    Updating setup.py to reference new requirements file

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='xqueue_watcher',
-    version='0.1',
+    version='0.2',
     description='XQueue Pull Grader',
     packages=[
         'xqueue_watcher',
     ],
-    install_requires=open('requirements.txt', 'rb').readlines()
+    install_requires=open('requirements/production.txt', 'rb').readlines()
 )
