ddf795b8211d14c662ff5a338377406febcef548
The Vinh Luong (Lương Thế Vinh) <TheVinhLuong@gmail.com>
2023-02-01 07:33:14 -0800

    Fix install_requires and add installation of grader_support package (#82)
    * fix install_requires (requirements/production.txt needs to be read as text), and add installation of grader_support package
    
    * minor edits (setup.py)

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
+
 setup(
     name='xqueue_watcher',
     version='0.2',
     description='XQueue Pull Grader',
     packages=[
+        'grader_support',
         'xqueue_watcher',
     ],
-    install_requires=open('requirements/production.txt', 'rb').readlines()
+    install_requires=open('requirements/production.txt',
+                          'rt', encoding='utf-8').readlines(),
 )
